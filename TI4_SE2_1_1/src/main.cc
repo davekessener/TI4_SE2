aa5532ab73d80540d7f7cafe3e4c21ee3c94b8c0
davekessener <davekessener@gmail.com>
2016-04-20 11:57:31 +0200

    included switch in led test

--- TI4_SE2_1_1/src/main.cc
+++ TI4_SE2_1_1/src/main.cc
@@ -8,10 +8,7 @@
 
 int main(int argc, char *argv[])
 {
-	lib::Thread ser1(test::serialA), ser2(test::serialB);
-
-	ser1.join();
-	ser2.join();
+	test::leds();
 
 	return 0;
 }
--- TI4_SE2_1_1/src/test/LEDTest.cc
+++ TI4_SE2_1_1/src/test/LEDTest.cc
@@ -18,8 +18,10 @@ namespace test
 		hw::LEDs::instance().blink(hw::LED::YELLOW, lib::Frequency::Hz(2.0));
 		hw::LEDs::instance().turnOn(hw::LED::GREEN);
 		delay.wait();
+		hw::Motors::instance().controlSwitch(hw::Motor::State::OPEN);
 		hw::LEDs::instance().turnOff(hw::LED::GREEN);
 		delay.wait();
+		hw::Motors::instance().controlSwitch(hw::Motor::State::CLOSE);
 		hw::LEDs::instance().turnOff(hw::LED::RED);
 
 		hw::Motors::instance().controlBelt(hw::Motor::Direction::RIGHT, hw::Motor::Speed::FAST);
