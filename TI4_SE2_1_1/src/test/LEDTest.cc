212d25ace51635ccef9e4d9b168d7ef7a1dec500
Malte Schuler <malte.schuler@haw-hamburg.de>
2016-04-20 12:31:36 +0200

    splitted LED and Actuator Test

--- TI4_SE2_1_1/src/test/LEDTest.cc
+++ TI4_SE2_1_1/src/test/LEDTest.cc
@@ -2,8 +2,6 @@
 #include "lib/TimeP.h"
 #include "hw/HWAccess.h"
 #include "hw/LED.h"
-#include "hw/Motor.h"
-#include "hw/Actuator.h"
 
 namespace test
 {
@@ -18,25 +16,17 @@ namespace test
 		hw::LEDs::instance().blink(hw::LED::YELLOW, lib::Frequency::Hz(2.0));
 		hw::LEDs::instance().turnOn(hw::LED::GREEN);
 		delay.wait();
-		hw::Motors::instance().controlSwitch(hw::Motor::State::OPEN);
 		hw::LEDs::instance().turnOff(hw::LED::GREEN);
 		delay.wait();
-		hw::Motors::instance().controlSwitch(hw::Motor::State::CLOSE);
 		hw::LEDs::instance().turnOff(hw::LED::RED);
 
-		hw::Motors::instance().controlBelt(hw::Motor::Direction::RIGHT, hw::Motor::Speed::FAST);
-
-		lib::Time::s(3).wait();
-
-		hw::Motors::instance().controlBelt(hw::Motor::Direction::LEFT, hw::Motor::Speed::SLOW);
+		lib::Time::s(1).wait();
 
 		std::cout << "commence blinking" << std::endl;
 
 		hw::LEDs::instance().blink(hw::LED::YELLOW, lib::Frequency::Hz(0.5));
 
 		lib::Time::s(6).wait();
-
-		hw::Motors::instance().controlBelt(hw::Motor::Direction::NONE, hw::Motor::Speed::STOP);
 
 		std::cout << "done blinking" << std::endl;
 
