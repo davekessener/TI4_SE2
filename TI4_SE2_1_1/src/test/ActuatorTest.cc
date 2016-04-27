b1e7b159585e3dc5e763fc61da5b95b09d2f3f59
Malte Schuler <malte.schuler@haw-hamburg.de>
2016-04-27 13:22:46 +0200

    Changed ActuatorTest

--- TI4_SE2_1_1/src/test/ActuatorTest.cc
+++ TI4_SE2_1_1/src/test/ActuatorTest.cc
@@ -9,40 +9,49 @@ namespace test
 {
 	void actuators(void)
 	{
-		lib::Time delay = lib::Time::s(1);
+		lib::Time delay = lib::Time::s(3);
 
-		hw::LEDs::instance().turnOn(hw::LED::RED);
-		hw::LEDs::instance().blink(hw::LED::YELLOW, lib::Frequency::Hz(2.0));
-		hw::LEDs::instance().turnOn(hw::LED::GREEN);
+        hw::LEDs::instance().turnOn(hw::LED::GREEN);
+        delay.wait();
+        hw::LEDs::instance().turnOff(hw::LED::GREEN);
+        hw::LEDs::instance().turnOn(hw::LED::YELLOW);
+        delay.wait();
+        hw::LEDs::instance().turnOff(hw::LED::YELLOW);
+        hw::LEDs::instance().turnOn(hw::LED::RED);
+        delay.wait();
+        hw::LEDs::instance().turnOff(hw::LED::RED);
+        hw::LEDs::instance().blink(hw::LED::GREEN, lib::Frequency::Hz(2.0));
+        delay.wait();
+        hw::LEDs::instance().blink(hw::LED::GREEN, lib::Frequency::Hz(0.5));
+        delay.wait();
+        hw::LEDs::instance().blink(hw::LED::YELLOW, lib::Frequency::Hz(2.0));
+        delay.wait();
+        hw::LEDs::instance().blink(hw::LED::YELLOW, lib::Frequency::Hz(0.5));
+        delay.wait();
+        hw::LEDs::instance().blink(hw::LED::RED, lib::Frequency::Hz(2.0));
+        delay.wait();
+        hw::LEDs::instance().blink(hw::LED::RED, lib::Frequency::Hz(0.5));
+        delay.wait();
+        hw::Motors::instance().controlBelt(hw::Motor::Direction::RIGHT, hw::Motor::Speed::FAST);
+        delay.wait();
+        hw::Motors::instance().controlBelt(hw::Motor::Direction::RIGHT, hw::Motor::Speed::SLOW);
+        delay.wait();
+        hw::Motors::instance().controlBelt(hw::Motor::Direction::RIGHT, hw::Motor::Speed::FAST);
+        delay.wait();
+        hw::Motors::instance().controlBelt(hw::Motor::Direction::RIGHT, hw::Motor::Speed::SLOW);
+        delay.wait();
+        hw::Motors::instance().controlBelt(hw::Motor::Direction::NONE, hw::Motor::Speed::STOP);
+        delay.wait();
+        hw::Motors::instance().controlSwitch(hw::Motor::State::OPEN);
+        delay.wait();
+        hw::Motors::instance().controlSwitch(hw::Motor::State::CLOSE);
 		hw::LEDs::instance().turnOn(hw::LED::START);
+        delay.wait();
 		hw::LEDs::instance().turnOn(hw::LED::RESET);
+        delay.wait();
 		hw::LEDs::instance().turnOn(hw::LED::Q1);
+        delay.wait();
 		hw::LEDs::instance().turnOn(hw::LED::Q2);
-		delay.wait();
-
-		hw::Motors::instance().controlSwitch(hw::Motor::State::OPEN);
-		hw::LEDs::instance().turnOff(hw::LED::GREEN);
-		delay.wait();
-
-		hw::Motors::instance().controlSwitch(hw::Motor::State::CLOSE);
-		hw::LEDs::instance().turnOff(hw::LED::RED);
-		hw::Motors::instance().controlBelt(hw::Motor::Direction::RIGHT, hw::Motor::Speed::FAST);
-		lib::Time::s(3).wait();
-
-		hw::Motors::instance().controlBelt(hw::Motor::Direction::LEFT, hw::Motor::Speed::SLOW);
-
-		hw::LEDs::instance().blink(hw::LED::YELLOW, lib::Frequency::Hz(0.5));
-
-		lib::Time::s(6).wait();
-
-		hw::Motors::instance().controlBelt(hw::Motor::Direction::NONE, hw::Motor::Speed::STOP);
-
-		hw::LEDs::instance().turnOff(hw::LED::YELLOW);
-		hw::LEDs::instance().turnOff(hw::LED::START);
-		hw::LEDs::instance().turnOff(hw::LED::RESET);
-		hw::LEDs::instance().turnOff(hw::LED::Q1);
-		hw::LEDs::instance().turnOff(hw::LED::Q2);
-
 		delay.wait();
 	}
 }
